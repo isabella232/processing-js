52a8783c77e5d9edfc82c3a152c15db1f511457b
Jared Forsyth <jared@Khans-MacBook-Pro.local>
2015-09-04 14:32:20 -0600

    add docs & such

--- processing.js
+++ processing.js
@@ -1392,23 +1392,44 @@
   ////////////////////////////////////////////////////////////////////////////
 
   /**
-   * A cache which has a limited size and removes the lease recently used
-   * items when that size is reached.
-   */
+  * This is a Least Recently Used Cache
+  *
+  * When the max size is reached, then the least recently used item is dropped.
+  *
+  * This is tracked by having a "use index", which is a number indicating how
+  * recently a given item was accessed. The closer the "use index" is to
+  * "mostRecent", the more recently is was used.
+  *
+  * When an item is accessed (via .get()) it's "use index" gets updated to be
+  * the new "most recent".
+  */
+
   function LRUCache(maxSize) {
     this.maxSize = maxSize || 100;
     this.size = 0;
     this.cache = {}; // key => val
     this.useIndex = {}; // use index => key
     this.useReverse = {}; // key => use index
-    this.mostRecent = -1;
-    this.leastRecent = -1;
+    this.mostRecent = 0;
+    this.leastRecent = 0;
   }
 
   /**
-   * Set a value in the cache. If the max size is reached, the least recently
-   * used item will be popped off.
-   */
+    * Get a value from the cache, returning undefined for an unknown key
+    */
+  LRUCache.prototype.get = function(key) {
+    key = key + '';
+    if (!this.cache[key]) {
+      return;
+    }
+    this._makeMostRecent(key);
+    return this.cache[key];
+  };
+
+  /**
+    * Set a value in the cache. If the max size is reached, the least recently
+    * used item will be popped off.
+    */
   LRUCache.prototype.set = function(key, val) {
     key = key + '';
     this.size += 1;
@@ -1420,21 +1441,14 @@
     }
   };
 
-  /**
-   * Get a value from the cache, returning undefined for an unknown key
-   */
-  LRUCache.prototype.get = function(key) {
-    key = key + '';
-    if (!this.cache[key]) {
-      return;
-    }
-    delete this.useIndex[this.useReverse[key]];
-
-    this._makeMostRecent(key);
-    return this.cache[key];
-  };
-
   LRUCache.prototype._makeMostRecent = function (key) {
+    var current = this.useReverse[key];
+    if (current === this.mostRecent) {
+      return;
+    } else if (current) {
+      delete this.useIndex[current];
+    }
+
     this.mostRecent += 1;
     var newIndex = this.mostRecent;
     this.useIndex[newIndex] = key;
